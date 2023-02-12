770d41849fed2dd96c0b6a4b59d9ed4b2d770198
nthduc <nguyenthaiduc0212@gmail.com>
2023-02-12 23:37:05 +0700

    updatw

---
+++ src/near-wallet.ts
@@ -0,0 +1,137 @@
+/* A helper file that simplifies using the wallet selector */
+
+// near api js
+import { providers } from 'near-api-js';
+
+// wallet selector UI
+import '@near-wallet-selector/modal-ui/styles.css';
+import { setupModal } from '@near-wallet-selector/modal-ui';
+import { setupCoin98Wallet } from "@near-wallet-selector/coin98-wallet";
+import { setupNearFi } from "@near-wallet-selector/nearfi";
+import { setupHereWallet } from "@near-wallet-selector/here-wallet";
+import { setupSender } from "@near-wallet-selector/sender";
+import { setupNearWallet } from "@near-wallet-selector/near-wallet";
+import { setupMathWallet } from "@near-wallet-selector/math-wallet";
+import { setupNightly } from "@near-wallet-selector/nightly";
+import { setupMeteorWallet } from "@near-wallet-selector/meteor-wallet";
+import { setupWelldoneWallet } from "@near-wallet-selector/welldone-wallet";
+import { setupNeth } from "@near-wallet-selector/neth";
+import { setupOptoWallet } from "@near-wallet-selector/opto-wallet";
+
+
+// wallet selector options
+import { ContractState, Network, NetworkId, setupWalletSelector, WalletType } from '@near-wallet-selector/core';
+import { setupLedger } from '@near-wallet-selector/ledger';
+import { setupMyNearWallet } from '@near-wallet-selector/my-near-wallet';
+
+
+const THIRTY_TGAS = '30000000000000';
+const NO_DEPOSIT = '0';
+
+// Wallet that simplifies using the wallet selector
+export class Wallet{
+  walletSelector: any;
+  wallet: any;
+  network: NetworkId | Network ;
+  createAccessKeyFor: any;
+    accountId?: any;
+
+  constructor({ createAccessKeyFor = undefined, network = 'testnet' }) {
+    // Login to a wallet passing a contractId will create a local
+    // key, so the user skips signing non-payable transactions.
+    // Omitting the accountId will result in the user being
+    // asked to sign all transactions.
+    this.createAccessKeyFor = createAccessKeyFor
+    this.network = network
+  }
+
+  // To be called when the website loads
+  async startUp() {
+    this.walletSelector = await setupWalletSelector({
+      network: this.network,
+      modules: [
+        setupMyNearWallet(),
+        setupLedger(),
+        setupCoin98Wallet(),
+        setupNearFi(),
+        setupHereWallet(),
+        setupSender(),
+        setupNearWallet(),
+        setupMathWallet(),
+        setupNightly(),
+        setupMeteorWallet(),
+        setupWelldoneWallet(),
+        setupNeth(),
+        setupOptoWallet()
+    ],
+    });
+
+    const isSignedIn = this.walletSelector.isSignedIn();
+
+    if (isSignedIn) {
+      this.wallet = await this.walletSelector.wallet();
+      this.accountId = this.walletSelector.store.getState().accounts[0].accountId;
+    }
+
+    return isSignedIn;
+  }
+
+  // Sign-in method
+  signIn() {
+    const description = 'Please select a wallet to sign in.';
+    const modal = setupModal(this.walletSelector, { contractId: this.createAccessKeyFor, description });
+    modal.show();
+  }
+
+  // Sign-out method
+  signOut() {
+    this.wallet.signOut();
+    this.wallet = this.accountId = this.createAccessKeyFor = null;
+    window.location.replace(window.location.origin + window.location.pathname);
+  }
+
+  // Make a read-only call to retrieve information from the network
+  async viewMethod({ contractId, method, args = {} }) {
+    const { network } = this.walletSelector.options;
+    const provider = new providers.JsonRpcProvider({ url: network.nodeUrl });
+
+    let res = await provider.query({
+      request_type: 'call_function',
+      account_id: contractId,
+      method_name: method,
+      args_base64: Buffer.from(JSON.stringify(args)).toString('base64'),
+      finality: 'optimistic',
+    });
+    return JSON.parse(Buffer.from(res.result).toString());
+  }
+
+  // Call a method that changes the contract's state
+  async callMethod({ contractId, method, args = {}, gas = THIRTY_TGAS, deposit = NO_DEPOSIT }) {
+    // Sign a transaction with the "FunctionCall" action
+    return await this.wallet.signAndSendTransaction({
+      signerId: this.accountId,
+      receiverId: contractId,
+      actions: [
+        {
+          type: 'FunctionCall',
+          params: {
+            methodName: method,
+            args,
+            gas,
+            deposit,
+          },
+        },
+      ],
+    });
+  }
+
+  // Get transaction result from the network
+  async getTransactionResult(txhash: string) {
+    const { network } = this.walletSelector.options;
+    const provider = new providers.JsonRpcProvider({ url: network.nodeUrl });
+
+    // Retrieve transaction result from the network
+    const transaction = await provider.txStatus(txhash, 'unnused');
+    return providers.getTransactionLastResult(transaction);
+  }
+}
